b76cd9514f5cfae23b0f5c1aabfde56a53758327
basu901 <basu901@gmail.com>
2025-08-21 23:31:06 -0700

    Weight Calculation

--- src/data_explore.py
+++ src/data_explore.py
@@ -104,17 +104,18 @@ def populate_avg_rating_table(connection_params:Dict):
     push_average_ratings(tuple(avg_rating.items()), connection_params)
 
 
-def find_users_for_movie(movie_id, connection_params):
+def find_users_and_rating_for_movie(movie_id, connection_params):
 
     with connect(**connection_params) as conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT user_id, rating  FROM movie_details WHERE movie_id=%s", (movie_id))
+            cur.execute("SELECT user_id, rating  FROM movie_details WHERE movie_id=%s", (movie_id,))
             rows = cur.fetchall()
     
     return rows
 
 
-def list_of_common_movies(user_j, user_i, connection_params):
+def ratings_from_common_movies(user_j, user_i, connection_params):
+
     with connect(**connection_params) as conn:
         with conn.cursor() as cur:
             cur.execute("""
@@ -123,16 +124,15 @@ def list_of_common_movies(user_j, user_i, connection_params):
 
                         WHERE a.user_id = %s AND b.user_id = %s 
                         """, (user_j, user_i))
-            row = cur.fetchone()
-            if row:
-                return f"Prediction Not Required. Rating is {row[0]}"
+            rows = cur.fetchall()
+    return rows
 
 
 
 def fetch_average(user_id, connection_params):
     with connect(**connection_params) as conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT rating_avg FROM avg_rating WHERE user_id=%s", (user_id))
+            cur.execute("SELECT rating_avg FROM avg_rating WHERE user_id=%s", (user_id,))
             row = cur.fetchone()
     return row[0]
 
@@ -149,8 +149,15 @@ def find_weights(user_i, user_j, connection_params):
         return None
 
 
-def predict_rating(user_id, movie_id, connection_params):
+def insert_weights(user_i, user_j, weight, connection_params):
+    with connect(**connection_params) as conn:
+            with conn.cursor() as cur:
+                cur.execute("INSERT INTO user_weights (user_id_1, user_id_2, weight) VALUES (%s, %s, %s)", (min(user_i, user_j), max(user_i, user_j), weight)) 
 
+
+
+def predict_rating(user_id, movie_id, connection_params):
+    import math
     with connect(**connection_params) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT rating FROM movie_details WHERE movie_id=%s AND user_id=%s", (movie_id, user_id))
@@ -160,6 +167,9 @@ def predict_rating(user_id, movie_id, connection_params):
 
     #This list will contain what the weight is for the user, rating given by the user, avg_rating given by the user
     user_j_info = list()
+
+    #The bias of user_id
+    avg_user_id = fetch_average(user_id, connection_params)
 
     #Find the list of users who have seen this movie       
     user_rating_list = find_users_and_rating_for_movie(movie_id, connection_params)
@@ -176,9 +186,30 @@ def predict_rating(user_id, movie_id, connection_params):
             user_j_info.append((user, rating_j, avg_j, weights))
         else:
             #Calculate and Store weights
-            common_movies = list_of_common_movies(user_id, user, connection_params)
+            #common_movies[i][0] refers to ratings for user_id
+            #common_movies[i][1] refers to ratings for user
+            #print(user)
+            common_movies = ratings_from_common_movies(user_id, user, connection_params)
+            ratings_df = pd.DataFrame(common_movies, columns=["user_id_rating", "user_rating"])
+            ratings_df['user_id_rating'] = ratings_df["user_id_rating"] - avg_user_id
+            ratings_df['user_rating'] = ratings_df["user_rating"] - avg_j
+            ratings_df['user_user_rating'] = ratings_df['user_id_rating'] * ratings_df['user_rating']
+            ratings_df['user_id_rating_squared'] = ratings_df["user_id_rating"]**2
+            ratings_df['user_rating_squared'] = ratings_df["user_rating"]**2
+            # print(ratings_df)
+            # input()
+            weight_params = ratings_df.sum(axis=0)
+            #print(weight_params)
+            weight = (weight_params['user_user_rating']/(math.sqrt(weight_params['user_id_rating_squared'])*math.sqrt(weight_params['user_rating_squared'])))
+            # print(weight)
+            # text = input()
+            # if text=="exit":
+            #     return
 
+            insert_weights(user_id, user, weight, connection_params)
+            user_j_info.append((user, rating_j, avg_j, weight))
 
+    
 
 if __name__=="__main__":
     connection_params ={
@@ -190,4 +221,5 @@ if __name__=="__main__":
     }
 
     #populate_tables()
-    populate_avg_rating_table(connection_params)
+    #populate_avg_rating_table(connection_params)
+    predict_rating(849,652, connection_params)
