aaf51847dd69e005b3e11f795c1dc9f87d95c04f
basu901 <basu901@gmail.com>
2025-08-23 10:43:27 -0700

    Added comments for functions

--- src/data_explore.py
+++ src/data_explore.py
@@ -15,6 +15,13 @@ pg_port = os.environ["POSTGRES_PORT"]
 
 
 def create_tables():
+    """
+    Creates the tables required:
+    avg_rating: Stores the bias of each user, by calculating the avg rating for all movies viewed by the user
+    movie_details: The master data which contains the rating given for each movie  by a particular user. 
+    user_weights: Stores the weights between two users
+    
+    """
     with connect(f"dbname={db} user={pg_user} password={pg_passwd} host={pg_host} port={pg_port}") as conn:
         with conn.cursor() as cur:
             cur.execute("""
@@ -44,12 +51,18 @@ def create_tables():
 
 
 def populate_movie_table(connection_params:Dict):
+    """
+    Populates the master table movie_data from the csv data.
+    
+    """
     from io import StringIO
     cur_dir = Path(__file__).cwd()
     dataset = cur_dir.joinpath("data","ml-100k","u_data.csv")
     df = pd.read_csv(str(dataset), header=None, names=['user_id','item_id','rating','timestamp'])
+
+    #Re-ordering to match the movie_data schema
     df_movie_details = df.iloc[:,[1,0,2]]
-    print(df_movie_details.head(3))
+    
     buffer = StringIO()
     df_movie_details.to_csv(buffer,header=False,index=True)
     buffer.seek(0)
@@ -63,7 +76,11 @@ def populate_movie_table(connection_params:Dict):
                     copy.write(data)
 
 
+
 def calculate_avg_rating(user_id:tuple, connection_params:Dict):
+    """
+    Calculates the average rating/bias for each user present in user_id
+    """
     with connect(**connection_params) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT rating FROM movie_details WHERE user_id=%s", user_id)
@@ -78,18 +95,14 @@ def calculate_avg_rating(user_id:tuple, connection_params:Dict):
     return round(total_rating/num_of_ratings,2)
 
 
-
-
-def push_average_ratings(avg_rating:tuple, connection_params:Dict):
-    
-    with connect(**connection_params) as conn:
-        with conn.cursor() as cur:
-            cur.executemany("INSERT INTO avg_rating (user_id, rating_avg) VALUES (%s, %s)", avg_rating) 
     
 
 
 
 def populate_avg_rating_table(connection_params:Dict):
+    """
+    Find average rating for each user
+    """
 
     avg_rating = {}
 
@@ -101,11 +114,20 @@ def populate_avg_rating_table(connection_params:Dict):
     for user in user_ids:
         avg_rating[user[0]] = calculate_avg_rating(user, connection_params)
 
-    push_average_ratings(tuple(avg_rating.items()), connection_params)
+    def push_average_ratings(avg_rating:tuple):
+        with connect(**connection_params) as conn:
+            with conn.cursor() as cur:
+                cur.executemany("INSERT INTO avg_rating (user_id, rating_avg) VALUES (%s, %s)", avg_rating) 
+
+    push_average_ratings(tuple(avg_rating.items()))
 
 
-def find_users_and_rating_for_movie(movie_id, connection_params):
 
+
+def find_users_and_rating_for_movie(movie_id:int, connection_params:Dict):
+    """
+    Find the list of users alongwith their ratings for a particular movie
+    """
     with connect(**connection_params) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT user_id, rating  FROM movie_details WHERE movie_id=%s", (movie_id,))
@@ -114,8 +136,12 @@ def find_users_and_rating_for_movie(movie_id, connection_params):
     return rows
 
 
-def ratings_from_common_movies(user_j, user_i, connection_params):
 
+
+def ratings_from_common_movies(user_j:int, user_i:int, connection_params:Dict):
+    """
+    Find the set of movies between two users and return the ratings given by both users, for each of the movies.
+    """
     with connect(**connection_params) as conn:
         with conn.cursor() as cur:
             cur.execute("""
@@ -129,7 +155,12 @@ def ratings_from_common_movies(user_j, user_i, connection_params):
 
 
 
-def fetch_average(user_id, connection_params):
+
+
+def fetch_average(user_id:int, connection_params:Dict):
+    """
+    Fetch avg rating for a particular user
+    """
     with connect(**connection_params) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT rating_avg FROM avg_rating WHERE user_id=%s", (user_id,))
@@ -138,7 +169,11 @@ def fetch_average(user_id, connection_params):
 
 
 
-def find_weights(user_i, user_j, connection_params):
+
+def find_weights(user_i:int, user_j:int, connection_params:Dict):
+    """
+    Fetch the weight between two users
+    """
     with connect(**connection_params) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT weight FROM user_weights WHERE user_id_1=%s AND user_id_2=%s", (min(user_i, user_j), max(user_i, user_j)))
@@ -149,15 +184,27 @@ def find_weights(user_i, user_j, connection_params):
         return None
 
 
-def insert_weights(user_i, user_j, weight, connection_params):
+
+
+def insert_weights(user_i:int, user_j:int, weight:float, connection_params:Dict):
+    """
+    Insert weight between two users into user_weights
+    """
     with connect(**connection_params) as conn:
             with conn.cursor() as cur:
                 cur.execute("INSERT INTO user_weights (user_id_1, user_id_2, weight) VALUES (%s, %s, %s)", (min(user_i, user_j), max(user_i, user_j), weight)) 
 
 
 
-def predict_rating(user_id, movie_id, connection_params):
+
+
+def predict_rating(user_id:int, movie_id:int, connection_params:Dict):
+    """
+    Predict the rating for a particular pair of (user, movie)
+    """
     import math
+
+    #Check if the user already has a rating for the movie
     with connect(**connection_params) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT rating FROM movie_details WHERE movie_id=%s AND user_id=%s", (movie_id, user_id))
@@ -165,7 +212,8 @@ def predict_rating(user_id, movie_id, connection_params):
             if row:
                 return f"Prediction Not Required. Rating is {row[0]}"
 
-    #This list will contain what the weight is for the user, rating given by the user, avg_rating given by the user
+    #user_j = has seen movie_id and has movies in common with user_id
+    #Stores info for user_j.Stores the weight, rating given for movie_id, avg_rating
     user_j_info = list()
 
     #The bias of user_id
@@ -180,15 +228,15 @@ def predict_rating(user_id, movie_id, connection_params):
         avg_j = fetch_average(user, connection_params)
 
         #Check if weights already exist
-        weights = find_weights(user_id, user, connection_params)
+        weight = find_weights(user_id, user, connection_params)
         
-        if weights:
-            user_j_info.append((user, rating_j, avg_j, weights))
+        if weight:
+            user_j_info.append((user, rating_j, avg_j, weight))
         else:
             #Calculate and Store weights
             #common_movies[i][0] refers to ratings for user_id
             #common_movies[i][1] refers to ratings for user
-            #print(user)
+            
             common_movies = ratings_from_common_movies(user_id, user, connection_params)
             ratings_df = pd.DataFrame(common_movies, columns=["user_id_rating", "user_rating"])
             ratings_df['user_id_rating'] = ratings_df["user_id_rating"] - avg_user_id
@@ -232,6 +280,8 @@ if __name__=="__main__":
         "port":f"{pg_port}"
     }
 
+    #Uncomment the following three lines, on the first pass of the code
+    #create_tables()
     #populate_tables()
     #populate_avg_rating_table(connection_params)
     print(predict_rating(849,652, connection_params))
